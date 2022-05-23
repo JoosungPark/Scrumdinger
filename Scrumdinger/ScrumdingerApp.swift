18f3b2a1c5e0b226323aa3cb346d805d707d495f
jay.park <jay.park@tosslab.com>
2022-05-23 15:05:54 +0900

    Create a Project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 박주성 on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
